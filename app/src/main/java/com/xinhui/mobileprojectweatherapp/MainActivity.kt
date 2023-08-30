6256ad1e465485696e8783880ce2a7e1ebceb84b
Adrian <Adriankzt05@gmail.com>
2023-08-30 15:28:46 +0800

    i did something

--- app/src/main/java/com/xinhui/mobileprojectweatherapp/MainActivity.kt
+++ app/src/main/java/com/xinhui/mobileprojectweatherapp/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.xinhui.mobileprojectweatherapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
