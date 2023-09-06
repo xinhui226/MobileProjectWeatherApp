52ff7090254e79dd8e3d5e31093cda1c05560fdf
TBNR-1257 <tzehernbryan22w@forwardschool.edu.my>
2023-09-06 10:00:01 +0800

    I do the search page and menu(in progress)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,14 @@
+import org.jetbrains.kotlin.konan.properties.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
+val propsFile = rootProject.file("local.properties")
+val localProps = Properties()
+localProps.load(FileInputStream(propsFile))
 
 android {
     namespace = "com.xinhui.mobileprojectweatherapp"
@@ -15,6 +22,9 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "API_URL", localProps["api_baseUrl"].toString())
+        buildConfigField("String", "API_KEY", localProps["apiKey"].toString())
     }
 
     buildTypes {
@@ -33,15 +43,33 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        dataBinding = true
+        buildConfig = true
+    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.fragment:fragment-ktx:1.6.1")
+
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:okhttp:3.14.9")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
+
+    implementation ("com.google.android.gms:play-services-location:21.0.1")
+
+    implementation("com.google.android.material:material:1.9.0")
 }
