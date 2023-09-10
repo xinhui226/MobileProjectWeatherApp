49c17577561cb77a369a2458bc6bc02918272530
Adrian <Adriankzt05@gmail.com>
2023-09-10 15:14:02 +0800

    Added all the features in the mainPage. Manage to show the location with their current weather and future forecast

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,10 +66,13 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
     implementation("androidx.fragment:fragment-ktx:1.6.1")
 
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("com.squareup.okhttp3:okhttp:3.14.9")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
-    implementation ("com.google.android.gms:play-services-location:21.0.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:3.14.9")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:glide:4.16.0")
 }
--- app/src/main/java/com/xinhui/mobileprojectweatherapp/ui/viewModels/HomeViewModel.kt
+++ app/src/main/java/com/xinhui/mobileprojectweatherapp/ui/viewModels/HomeViewModel.kt
@@ -1,7 +1,67 @@
 package com.xinhui.mobileprojectweatherapp.ui.viewModels
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.xinhui.mobileprojectweatherapp.MyApplication
+import com.xinhui.mobileprojectweatherapp.data.model.CurrentWeatherDisplay
+import com.xinhui.mobileprojectweatherapp.data.model.ForecastWeatherDisplay
+import com.xinhui.mobileprojectweatherapp.data.repository.RetrofitRepo
+import com.xinhui.mobileprojectweatherapp.ui.util.Constant.TAG
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel(
+    private val repo: RetrofitRepo
+) : ViewModel() {
 
+    val currWeather: MutableStateFlow<CurrentWeatherDisplay> = MutableStateFlow(
+        CurrentWeatherDisplay()
+    )
+
+    val forecastWeather: MutableStateFlow<List<ForecastWeatherDisplay>> = MutableStateFlow(
+        listOf(ForecastWeatherDisplay())
+    )
+
+    init {
+        showCurrentWeather()
+        showForecastWeather()
+    }
+
+    fun showCurrentWeather() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                currWeather.value = repo.getCurrWeather("Kampung Kok, Malaysia")
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun showForecastWeather() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                forecastWeather.value = repo.getForecastWeather("Kampung Kok, Malaysia")
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val retrofitRepository =
+                    (this[APPLICATION_KEY] as MyApplication).retrofitRepo
+                HomeViewModel(
+                    repo = retrofitRepository,
+                )
+            }
+        }
+    }
 }
