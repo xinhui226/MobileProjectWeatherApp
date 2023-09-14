ec610b3cd1bfecc615cd3662fef5edcec70015ba
Xin Hui <xinhui@globalconnect@gmail.com>
2023-09-14 10:17:12 +0800

    remove save location function and retrofit repo in EditDeleteViewModel

--- app/src/main/java/com/xinhui/mobileprojectweatherapp/ui/viewModels/EditDeleteViewModel.kt
+++ app/src/main/java/com/xinhui/mobileprojectweatherapp/ui/viewModels/EditDeleteViewModel.kt
@@ -1,7 +1,5 @@
 package com.xinhui.mobileprojectweatherapp.ui.viewModels
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
@@ -10,8 +8,6 @@ import androidx.lifecycle.viewmodel.viewModelFactory
 import com.xinhui.mobileprojectweatherapp.MyApplication
 import com.xinhui.mobileprojectweatherapp.data.model.Location
 import com.xinhui.mobileprojectweatherapp.data.repository.LocationRepo
-import com.xinhui.mobileprojectweatherapp.data.repository.RetrofitRepo
-import com.xinhui.mobileprojectweatherapp.ui.util.localtimeFormatter
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
@@ -19,7 +15,6 @@ import kotlinx.coroutines.launch
 
 class EditDeleteViewModel(
     val locationRepo: LocationRepo,
-    val retrofitRepo: RetrofitRepo,
 ) : ViewModel() {
 
     private lateinit var firstLocation:Location
@@ -49,42 +44,14 @@ class EditDeleteViewModel(
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun addLocation(){
-        viewModelScope.launch(Dispatchers.IO){
-            val location = retrofitRepo.getCurrWeather("Gurun,MY")
-            locationRepo.savedLocation(
-                Location(city = location.cityName,
-                    temp = location.temp,
-                    localtime = localtimeFormatter(location.timezone,location.timestamp),
-                    weatherDesc = location.weather.description))
-            val location2 = retrofitRepo.getCurrWeather("SungaiPetani,MY")
-            locationRepo.savedLocation(
-                Location(city = location2.cityName,
-                    temp = location2.temp,
-                    localtime = localtimeFormatter(location2.timezone,location2.timestamp),
-                    weatherDesc = location2.weather.description))
-            val location3 = retrofitRepo.getCurrWeather("KualaLumpur,MY")
-            locationRepo.savedLocation(
-                Location(city = location3.cityName,
-                    temp = location3.temp,
-                    localtime = localtimeFormatter(location3.timezone,location3.timestamp),
-                    weatherDesc = location3.weather.description))
-        }
-    }
-
     companion object {
         val Factory: ViewModelProvider.Factory = viewModelFactory {
             initializer {
                 val locationRepository =
                     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MyApplication)
                         .locationRepo
-                val weatherRepository =
-                    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MyApplication)
-                        .retrofitRepo
                 EditDeleteViewModel(
                     locationRepo = locationRepository,
-                    retrofitRepo = weatherRepository,
                 )
             }
         }
